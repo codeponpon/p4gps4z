df868311b60108a16ba8fd5f81be97de5697f2cc
codePONPON <codeponpon@gmail.com>
2013-10-08 19:36:00 +0700

    cap install

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,39 +40,53 @@
 # end
 
 require "bundler/capistrano"
- 
+
+
 set :application, "pagpos"
 set :user, "pagposazure"
- 
+
+
 set :scm, :git
-# set :repo_url, 'git@github.com:codeponpon/p4gps4z.git'
-set :repository, "https://github.com/codeponpon/p4gps4z.git"
+set :repository, "git@github.com:codeponpon/p4gps4z.git"
 set :branch, "master"
 set :use_sudo, true
- 
-server "138.91.40.59", :web, :app, :db, primary: true
- 
-set :deploy_to, "/home/#{user}/apps/#{application}"
+
+
+server "pagposv1.cloudapp.net", :web, :app, :db, primary: true
+
+
+set :deploy_to, "/home/#{user}/repositories/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
- 
+ssh_options[:port] = SSHPort
+
+
 namespace :deploy do
+  desc "Fix permissions"
+  task :fix_permissions, :roles => [ :app, :db, :web ] do
+    run "chmod +x #{release_path}/config/unicorn_init.sh"
+  end
+
+
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "service unicorn_#{application} #{command}"
     end
   end
- 
+
+
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     sudo "mkdir -p #{shared_path}/config"
   end
   after "deploy:setup", "deploy:setup_config"
- 
+
+
   task :symlink_config, roles: :app do
     # Add database config here
   end
+  after "deploy:finalize_update", "deploy:fix_permissions"
   after "deploy:finalize_update", "deploy:symlink_config"
 end
