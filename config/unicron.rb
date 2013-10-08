a94380d8a626f7b7ddaec35c787ba9dee1f6ac51
codePONPON <codeponpon@gmail.com>
2013-10-08 23:06:39 +0700

    change app path

--- config/unicron.rb
+++ config/unicron.rb
@@ -1,4 +1,4 @@
-root = "/home/pagposazure/apps/pagpos/releases"
+root = "/home/pagposazure/apps/pagpos/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
