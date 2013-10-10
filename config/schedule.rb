053f1bef9e27251e64155a6fefedaccc5881c61d
codePONPON <codeponpon@gmail.com>
2013-10-10 13:45:57 +0700

    change config

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,7 +24,7 @@
 # every :day, :at => '12:20am', :roles => [:app]
 
 set :output, File.join(Dir.getwd, 'tmp', 'logs', 'cron_log.log')
-set :environment, Rails.env || ENV['RAILS_ENV'] || "development"
+set :environment, ENV['RAILS_ENV'] || "development"
 
 every :reboot do
   # command "whenever -i #{File.join(Dir.getwd, 'config', 'schedule.rb')}"
