5668b69cb996743380372bf2c6ec5d3daf5f5d07
codePONPON <codeponpon@gmail.com>
2013-10-18 22:08:38 +0700

    response tracking data for api

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,10 +3,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
     
     if @user.persisted?
-      # flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      # sign_in @user
-      # sign_in_and_redirect @user, :event => :authentication
-      return render :json=> {:success => true, :token => @user.id.to_s}
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = env["omniauth.auth"]
       redirect_to new_user_registration_url
