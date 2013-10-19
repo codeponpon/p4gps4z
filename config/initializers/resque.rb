61feae183b0231f0d27a2cea31badb9acc87c12d
codePONPON <codeponpon@gmail.com>
2013-10-19 22:09:32 +0700

    add authentication to resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,8 @@
+Resque::Server.use Rack::Auth::Basic do |username, password|
+  username == "codeponpon"
+  password == "c0d3p0np0n"
+end
+
 Dir[File.join(Rails.root, 'app', 'workers', '*.rb')].each { |file| require file if file.present? }
 
 config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
