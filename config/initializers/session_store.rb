ee692ab6666ee6273c07bb077cb8e31777352da4
codePONPON <codeponpon@gmail.com>
2013-10-24 22:21:16 +0700

    set session expire if inactive more than 30 minute

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,1 @@
-# Be sure to restart your server when you modify this file.
-if ["production","staging"].include?(Rails.env)
-  Pagpos::Application.config.session_store :cookie_store,
-    # :memcache_server => (Rails.env.production? ? ['IP_MEMCHACH_SERVER'] : ['localhost']),
-    :namespace => 'sessions',
-    :key => '_pagpos_session',
-    :expire_after => 1.minutes,
-    :domain => :all
-else
-  Pagpos::Application.config.session_store :cookie_store, key: '_pagpos_session'
-end
+Pagpos::Application.config.session_store :cookie_store, key: '_pagpos_session'
