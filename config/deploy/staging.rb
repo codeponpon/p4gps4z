9cd379d78d6ae8e9d20ce154ee952f795d912c63
codePONPON <codeponpon@gmail.com>
2013-11-13 23:55:53 +0700

    - config separated deploy branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,12 +3,4 @@ server "pagposdev.cloudapp.net", :web, :app, :worker
 
 set :repository, "git@github.com:codeponpon/p4gps4z.git"
 set :branch, "staging"
-set :use_sudo, true
 
-set :branch do
-  default_tag = `git tag`.split("\n").last
-
-  tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the tag first): [#{default_tag}] "
-  tag = default_tag if tag.empty?
-  tag
-end
