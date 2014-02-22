ba58f93286f73f43760c02f310e1c50ccbb8d089
codePONPON <codeponpon@gmail.com>
2014-02-22 15:49:28 +0700

    - fix redirect - add logo into store page - add default backend menu

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,7 +1,7 @@
 class CustomFailure < Devise::FailureApp
   def redirect_url
     if warden_options[:scope] == :user
-      request.referer
+      request.referer || root_path
     else
       root_path
     end
