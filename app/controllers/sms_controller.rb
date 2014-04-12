f21f65d0b8935363c986fd7561a8e39691076d89
codePONPON <codeponpon@gmail.com>
2014-04-12 17:51:31 +0700

    sms show detail

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -10,9 +10,11 @@ class SmsController < ApplicationController
       @credit += cu.campaign.credit
     end
 
-    @trackings = current_user.trackings
+    @credit_used = 0;
     @trackings.each do |t|
-      t.packages
+      t.packages.each do |p|
+        @credit_used += 1 if p.notification
+      end
     end
 
   end
@@ -25,21 +27,44 @@ class SmsController < ApplicationController
   private
     def search_by
       params[:filter] = 'day' unless params[:filter]
+      @trackings = []
+      @customer_count = 0;
       case params[:filter]
       when 'all'
         @campaigns = current_user.campaigns_users.all
+        current_user.customers.each do |c|
+          @trackings += c.trackings.all
+        end
       when 'week'
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_week, :created_at.lte => DateTime.now.end_of_week)
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_week, :created_at.lte => DateTime.now.end_of_week)
+        end
       when 'month'
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month)
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month)
+        end
       when '3months'
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month.advance(months: +3))
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month.advance(months: +3))
+        end
       when '6months'
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month.advance(months: +6))
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_month, :created_at.lte => DateTime.now.end_of_month.advance(months: +6))
+        end
       when 'year'
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_year, :created_at.lte => DateTime.now.end_of_year)
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_year, :created_at.lte => DateTime.now.end_of_year)
+        end
       else
         @campaigns = current_user.campaigns_users.where(:created_at.gte => DateTime.now.beginning_of_day, :created_at.lte => DateTime.now.end_of_day)
+        current_user.customers.each do |c|
+          @trackings += c.trackings.where(:created_at.gte => DateTime.now.beginning_of_day, :created_at.lte => DateTime.now.end_of_day)
+        end
       end
     end
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -4,12 +4,14 @@ class Package
   belongs_to :tracking
   embeds_one :image
 
-  field :process_at,  :type => String 
+  field :process_at,  :type => String
   field :department,  :type => String
   field :description, :type => String
   field :status,      :type => String
   field :reciever,    :type => String
   field :signature,   :type => String
+  field :notification,:type => String, default: false
+  field :notification_by,:type => String, default: ''
 
   def save_file_from_url(url)
     self.image = Image.new(attachment: URI.parse(url)) unless url.blank?
