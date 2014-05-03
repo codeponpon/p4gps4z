8f81a098e41da9cd220872caa5792520e6f05720
codePONPON <codeponpon@gmail.com>
2014-05-03 22:53:22 +0700

    add invoice index

--- app/controllers/pag_invoices_controller.rb
+++ app/controllers/pag_invoices_controller.rb
@@ -3,6 +3,8 @@ class PagInvoicesController < ApplicationController
 
   def index
     @page_title = I18n.t('page_title.invoices')
+    page = params[:page].present? ? params[:page] : 1
+    @invoices = current_user.pag_invoices.paginate(:page => page, :per_page => 20)
   end
 
   def show
