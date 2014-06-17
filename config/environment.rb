47e2b1a8f14c8450ba957c9c32288c058afb1c9e
codePONPON <codeponpon@gmail.com>
2014-06-18 00:20:49 +0700

    change to paypla sdk

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'paypal'
+require 'paypal-sdk-rest'
 
 # Initialize the Rails application.
 Pagpos::Application.initialize!
