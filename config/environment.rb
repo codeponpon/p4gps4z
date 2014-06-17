016330fdc2fd13f60b7eff89571b5eafc31eb96c
codePONPON <codeponpon@gmail.com>
2014-06-18 00:25:59 +0700

    remove require paypal add require to initial load

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'paypal-sdk-rest'
 
 # Initialize the Rails application.
 Pagpos::Application.initialize!
--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,4 @@
+require 'paypal-sdk-rest'
+
 PayPal::SDK.load("config/paypal.yml", Rails.env)
 PayPal::SDK.logger = Rails.logger
